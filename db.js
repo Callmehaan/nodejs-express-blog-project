f610363f7988a28952f401166851dc1c1c13e8ba
Mahan <121290805+Callmehaan@users.noreply.github.com>
2024-09-23 18:25:24 +0330

    feat: define db relations in sequelize

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 const { Sequelize } = require("sequelize");
 const configs = require("./configs");
 
+const User = require("./models/User");
+const Tag = require("./models/Tag");
+const Article = require("./models/Article");
+const TagArticle = require("./models/TagArticle");
+
 const db = new Sequelize({
     host: configs.db.host,
     port: configs.db.port,
@@ -9,6 +14,26 @@ const db = new Sequelize({
     database: configs.db.name,
     dialect: configs.db.dialect,
     logging: configs.isProduction ? false : console.log,
+});
+
+User.hasMany(Article, {
+    foreignKey: "author_id",
+    onDelete: "CASCADE",
+});
+Article.belongsTo(User, {
+    foreignKey: "author_id",
+    as: "author",
+});
+
+Article.belongsToMany(Tag, {
+    through: TagArticle,
+    onDelete: "CASCADE",
+    foreignKey: "article_id",
+});
+Tag.belongsToMany(Article, {
+    through: TagArticle,
+    onDelete: "CASCADE",
+    foreignKey: "tag_id",
 });
 
 module.exports = db;
